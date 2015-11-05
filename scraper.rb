3ce16cbdf46f4b5e7c61bb15b00cdc813176cd0d
jpalmieri <jpalmieri@lumoslabs.com>
2015-11-05 14:27:05 -0800

    encode query string

--- scraper.rb
+++ scraper.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require 'cgi'
 
 BASE_URL = 'https://www.ultimate-guitar.com/search.php'
 
@@ -17,7 +18,8 @@ class Search
   include Scraper
 
   def initialize(query)
-    page_url = "#{BASE_URL}?search_type=title&value=#{query})"
+    query_string = CGI::escape(query)
+    page_url = "#{BASE_URL}?search_type=title&value=#{query_string})"
     @output = self.get_page(page_url)
   end
 
